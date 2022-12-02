3276dcde6e9bc62a93973b6209292b12f02b77b1
patilprks <patilprks@gmail.com>
2022-12-02 14:21:57 +0530

    useReducer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+# React Hooks
 # Hooks are a new addition in React 16.8. They let you use state and other React features
 # Learn how to use React Hooks with example.
--- src/App.js
+++ src/App.js
@@ -1,52 +1,98 @@
-import './App.css';
-import {useState} from 'react';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Chip from '@mui/material/Chip';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
-import CardContent from '@mui/material/CardContent';
+import "./App.css";
+import { useState, useReducer } from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import CardContent from "@mui/material/CardContent";
 
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return {count: state.count + 1 , showText: state.showText}
+    case 'toggleShowText':
+      return {count: state.count, showText: !state.showText}
+    default:
+      return {state}  
+  }
+
+}
 
 <Box
-component="span"
-sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
->
-•
-</Box>
+  component="span"
+  sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
+  •
+</Box>;
 
 function App() {
-//  useState example 1
-const [counter, setCounter] = useState(0);
-const [inputValue, setInputValue] = useState('Patil');
+  //  useState example 1
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("Patil");
 
-const incrementCounter = () => {
-  setCounter(counter + 1);
-}
+  const [state, dispatch] = useReducer(reducer, {count: 0, showText: true});
 
- const captureValues = (event) => {
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const captureValues = (event) => {
     const newValue = event.target.value;
     setInputValue(newValue);
-}
+  };
 
   return (
     <div className="App">
-    <h1>React Hooks</h1>
-    <h3>1. useState()</h3>
-    {/* useState example 1 */}
-     <Card sx={{ minWidth: 50 }}>
-     <CardContent>
-     
-      <div> <Chip label={counter} variant="outlined"/> <Button className="ml-15" variant="contained" onClick={incrementCounter}>Increament</Button> </div>
-      </CardContent>
-      </Card>
-      <Divider className="mr"/>
+      <h1>React Hooks</h1>
+      <p>1. useState()</p>
+      {/* useState example 1 */}
       <Card sx={{ minWidth: 50 }}>
-     <CardContent>
-     <TextField id="outlined-basic" label="Text" variant="outlined" onChange={captureValues}/> 
-     <div className="mt-15">{inputValue} </div>
-      </CardContent>
+        <CardContent>
+          <div>
+            <Chip label={counter} variant="outlined" />
+            <Button
+              className="ml-15"
+              variant="contained"
+              onClick={incrementCounter}
+            >
+              Increament
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+      <Divider className="mr" />
+      <Card sx={{ minWidth: 50 }}>
+        <CardContent>
+          <TextField
+            id="outlined-basic"
+            label="Text"
+            variant="outlined"
+            onChange={captureValues}
+          />
+          <div className="mt-15">{inputValue} </div>
+        </CardContent>
+      </Card>
+      <Divider className="mr" />
+      <p>2. useReducer()</p>
+      {/* useState example 1 */}
+      <Card sx={{ minWidth: 50 }}>
+        <CardContent>
+          <div>
+            <Chip label={state.count} variant="outlined" />
+            <Button
+              className="ml-15"
+              variant="contained"
+              onClick={()=>{
+                dispatch({type: "INCREMENT"});
+                dispatch({type: "toggleShowText"});
+              }}
+            >
+              Increament
+            </Button>
+            {state.showText && <p>This is a text</p>}
+          </div>
+        </CardContent>
       </Card>
     </div>
   );
